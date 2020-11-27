282418ba06ddb3b710ee5291751ef4c6ac1f04ce
christianbastin <christianbastin@users.noreply.github.com>
2020-11-27 12:33:54 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ import {Observable} from 'rxjs/Observable';
 export class ProductService {
 
   private _albumUrl  = '../assets/album.json';
-  private _productsUrl = '../assets/products.json ';
+  private _productsUrl  = '../assets/products.json ';
 
   constructor(private _http: Http) { }
 
@@ -16,7 +16,7 @@ export class ProductService {
      return this._http.get(this._albumUrl).map((response) => <Album> response.json());
   }
 
-  getProduct(): Observable<Product[]> {
+  getProducts(): Observable<Product[]> {
     return this._http.get(this._productsUrl).map((response) => <Product[]> response.json());
   }
 }
